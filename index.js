28c68227ed39439fef09a460e49b1ba2b105bd7f
Vinicius Barreto <vinicius.s.barreto@gmail.com>
2026-04-22 18:26:18 -0300

    Init Exercises of Module 2
    This commit aims to create an initial deep learning model using TensorFlow in order to classify students based on a fixed labels.

---
+++ index.js
@@ -0,0 +1,147 @@
+import tf from '@tensorflow/tfjs-node';
+
+
+async function trainModel(inputXs, outputYs) {
+    const model = tf.sequential()
+
+    // Primeira camada da rede:
+    // entrada de 7 posições (idade normalizada + 3 cores + 3 localizacoes)
+
+    // 80 neuronios = aqui coloquei tudo isso, pq tem pouca base de treino
+    // quanto mais neuronios, mais complexidade a rede pode aprender
+    // e consequentemente, mais processamento ela vai usar
+
+    // A ReLU age como um filtro:
+    // É como se ela deixasse somente os dados interessantes seguirem viagem na rede
+    // Se a informação chegou nesse neuronio é positiva, passa para frente!
+    // se for zero ou negativa, pode jogar fora, nao vai servir para nada
+    model.add(tf.layers.dense({ inputShape: [7], units: 80, activation: 'relu' }))
+
+    // Saída: 3 neuronios
+    // um para cada categoria (premium, medium, basic)
+
+    // activation: softmax normaliza a saida em probabilidades
+    model.add(tf.layers.dense({ units: 3, activation: 'softmax' }))
+
+    // Compilando o modelo
+    // optimizer Adam ( Adaptive Moment Estimation)
+    // é um treinador pessoal moderno para redes neurais:
+    // ajusta os pesos de forma eficiente e inteligente
+    // aprender com historico de erros e acertos
+
+    // loss: categoricalCrossentropy
+    // Ele compara o que o modelo "acha" (os scores de cada categoria)
+    // com a resposta certa
+    // a categoria premium será sempre [1, 0, 0]
+
+    // quanto mais distante da previsão do modelo da resposta correta
+    // maior o erro (loss)
+    // Exemplo classico: classificação de imagens, recomendação, categorização de
+    // usuário
+    // qualquer coisa em que a resposta certa é "apenas uma entre várias possíveis"
+
+    model.compile({
+        optimizer: 'adam',
+        loss: 'categoricalCrossentropy',
+        metrics: ['accuracy']
+    })
+
+    // Treinamento do modelo
+    // verbose: desabilita o log interno (e usa só callback)
+    // epochs: quantidade de veses que vai rodar no dataset
+    // shuffle: embaralha os dados, para evitar viés
+    await model.fit(
+        inputXs,
+        outputYs,
+        {
+            verbose: 0,
+            epochs: 100,
+            shuffle: true,
+            callbacks: {
+                onEpochEnd: (epoch, log) => console.log(
+                    `Epoch: ${epoch}: loss = ${log.loss}`
+                )
+            }
+        }
+    )
+
+    console.log(`\n`)
+
+    return model
+}
+
+async function predict(model, person) {
+    // transformar o array js para o tensor (tfjs)
+    const tfInput = tf.tensor2d(person)
+
+    // Faz a predição (output será um vetor de 3 probabilidades)
+    const prediction = model.predict(tfInput)
+    const predictionArray = await prediction.array()
+
+    return predictionArray[0].map((probability, index) => ({ probability, index }))
+}
+
+// Exemplo de pessoas para treino (cada pessoa com idade, cor e localização)
+// const pessoas = [
+//     { nome: "Erick", idade: 30, cor: "azul", localizacao: "São Paulo" },
+//     { nome: "Ana", idade: 25, cor: "vermelho", localizacao: "Rio" },
+//     { nome: "Carlos", idade: 40, cor: "verde", localizacao: "Curitiba" }
+// ];
+
+// Vetores de entrada com valores já normalizados e one-hot encoded
+// Ordem: [idade_normalizada, azul, vermelho, verde, São Paulo, Rio, Curitiba]
+// const tensorPessoas = [
+//     [0.33, 1, 0, 0, 1, 0, 0], // Erick
+//     [0, 0, 1, 0, 0, 1, 0],    // Ana
+//     [1, 0, 0, 1, 0, 0, 1]     // Carlos
+// ]
+
+// Usamos apenas os dados numéricos, como a rede neural só entende números.
+// tensorPessoasNormalizado corresponde ao dataset de entrada do modelo.
+const tensorNormalizedPeople = [
+    [0.33, 1, 0, 0, 1, 0, 0], // Erick
+    [0, 0, 1, 0, 0, 1, 0],    // Ana
+    [1, 0, 0, 1, 0, 0, 1]     // Carlos
+]
+
+// Labels das categorias a serem previstas (one-hot encoded)
+// [premium, medium, basic]
+const labelsNomes = ["Premium", "Medium", "Basic"]; // Ordem dos labels
+const tensorLabels = [
+    [1, 0, 0], // premium - Erick
+    [0, 1, 0], // medium - Ana
+    [0, 0, 1]  // basic - Carlos
+];
+
+// Criamos tensores de entrada (xs) e saída (ys) para treinar o modelo
+const inputXs = tf.tensor2d(tensorNormalizedPeople)
+const outputYs = tf.tensor2d(tensorLabels)
+
+// quanto mais dado melhor!
+// assim o algoritmo consegue entender melhor os padrões complexos
+// dos dados
+const model = await trainModel(inputXs, outputYs)
+
+const person = { nome: 'Ana', idade: 32, cor: 'verde', localizacao: "Sao Paulo" }
+// Normalizando a idade da nova pessoa usando o mesmo padrão do treino
+// Exemplo: idade_min = 25, idade_max = 40, então (28 - 25) / (40 - 25 ) = 0.2
+
+const tensorNormalizedPerson = [
+    [
+        0.3, // idade normalizada
+        1,    // cor azul
+        0,    // cor vermelho
+        0,    // cor verde
+        1,    // localização São Paulo
+        0,    // localização Rio
+        0     // localização Curitiba
+    ]
+]
+
+const predictions = await predict(model, tensorNormalizedPerson)
+const results = predictions
+    .sort((a, b) => b.probability - a.probability)
+    .map(p => `${labelsNomes[p.index]} (${(p.probability * 100).toFixed(4)}%)`)
+    .join('\n')
+
+console.log(results)
