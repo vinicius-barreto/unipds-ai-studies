977796f671680e6e0318af3d4757d56028ec8fb5
Vinicius Barreto <vinicius.s.barreto@gmail.com>
2026-05-07 21:55:31 -0300

    Automatize the Duck Kills
    This commit aims to automatize the game in order to automatically kill the ducks based on screenshot predictions.

--- duck-hunter-game/machine-learning/worker.js
+++ duck-hunter-game/machine-learning/worker.js
@@ -4,6 +4,7 @@ const MODEL_PATH = `yolov5n_web_model/model.json`;
 const LABELS_PATH = `yolov5n_web_model/labels.json`;
 const INPUT_MODEL_DIMENSION_WIDTH = 640
 const INPUT_MODEL_DIMENSION_HEIGHT = 640
+const SCORE_THRESHOLD = 0.4
 
 let _labels = []
 let _model = null
@@ -66,6 +67,44 @@ async function runInference(tensor) {
     }
 }
 
+/**
+ * Filtra e processa as predições:
+ * - Aplica o limiar de confiança (CLASS_THRESHOLD)
+ * - Filtra apenas a classe desejada (exemplo: 'kite')
+ * - Converte coordenadas normalizadas para pixels reais
+ * - Calcula o centro do bounding box
+ *
+ * Uso de generator (function*):
+ * - Permite enviar cada predição assim que processada, sem criar lista intermediária
+ */
+function* processPrediction({boxes, scores, classes}, width, height) {
+    for(let index = 0; index < scores.length; index++) {
+        if(scores[index] < SCORE_THRESHOLD) continue
+        
+        const label = _labels[classes[index]]
+        if(label !== 'kite') continue
+
+        let [x1, y1, x2, y2] = boxes.slice(index * 4, (index + 1) * 4)
+        x1 *= width
+        x2 *= width
+        y1 *= height
+        y2 *= height
+
+        const boxWidth = x2 - x1
+        const boxHeight = y2 -y1
+        
+        const centerX = x1 + boxWidth / 2
+        const centerY = y1 + boxHeight / 2
+
+        yield {
+            x: centerX,
+            y: centerY,
+            score: (scores[index] * 100).toFixed(2)
+        }
+
+    }
+}
+
 loadModelAndLabels()
 
 self.onmessage = async ({ data }) => {
@@ -77,14 +116,12 @@ self.onmessage = async ({ data }) => {
     const { width, height } = data.image
     const inferenceResults = await runInference(input)
 
-    debugger
-    postMessage({
-        type: 'prediction',
-        x: 400,
-        y: 400,
-        score: 0
-    });
-
+    for (const prediction of processPrediction(inferenceResults, width, height)) {
+        postMessage({
+            type: 'prediction',
+            ...prediction
+        });
+    }
 
 };
 
